256e74ec82a7ff91c5c5b90e9be5ea730ccf43da
IanCarlosLT <xxxiancarlosxxx@gmail.com>
2025-06-25 14:20:23 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 
 func main() {
 	db, err := sql.Open("postgres", "user=postgres dbname=herguardian sslmode=disable password=apolo777")
-	err = db.Ping()
 	if err != nil {
+		log.Fatal("Erro ao abrir conexão com o banco:", err)
+	}
+	if err = db.Ping(); err != nil {
 		log.Fatal("Erro ao testar conexão com o banco:", err)
 	}
 
@@ -22,19 +24,18 @@ func main() {
 	http.HandleFunc("/ficha/dados-anamnese", handlers.Anamnese(db))
 	http.HandleFunc("/ficha/exame-clinico", handlers.Exame_clinico(db))
 	http.HandleFunc("/ficha/exame-laboratorial", handlers.Exame_laboratorial(db))
+	http.HandleFunc("/api/pacientes", handlers.BuscarPacienteHandler(db))
+	http.HandleFunc("/criar-conta", handlers.CriarContaHandler(db))
+	http.HandleFunc("/login", handlers.LoginHandler(db))
+	http.HandleFunc("/perfil", handlers.PerfilHandler(db))
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/escolha-login.html", http.StatusSeeOther)
+	})
 
 	porta := ":8080"
 	log.Println("Servidor rodando em http://localhost" + porta)
 	log.Fatal(http.ListenAndServe(porta, nil))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/Front-end/escolha-login.html", http.StatusSeeOther)
-	})
-
 }
