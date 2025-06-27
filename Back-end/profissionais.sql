2291c15e3165ab0a75df45ec2e91ae7be3664661
IanCarlosLT <xxxiancarlosxxx@gmail.com>
2025-06-27 11:19:20 -0300

    Update profissionais.sql

---
+++ Back-end/profissionais.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS public-profissionais
+  id SERIAL PRIMARY KEY, 
+  nome TEXT NOT NULL,
+  cpf TEXT UNIQUE NOT NULL, 
+  codigo_autorizacao TEXT NOT NULL, 
+  telefone TEXT,
+  email TEXT UNIQUE NOT NULL, 
+  cep TEXT, 
+  crm_coren TEXT, 
+  funcao TEXT, 
+  senha TEXT NOT NULL, 
+  criado_em TIMESTAMP DEFAULT CURRENT_ TIMPESTAMP
+)
